457dee0e85663378d77408247accaefc9cf682fc
Marcelo Galvão <mgalvao@salesforce.com>
2020-06-04 10:52:56 -0300

    configuracao da conexao com mysql

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  HOST: "us-cdbr-iron-east-02.cleardb.net",
-  USER: "b7e24378878xxx",
-  PASSWORD: "0200exxx",
-  DB: "heroku_7643ec736354xxx"
+  HOST: "us-cdbr-east-05.cleardb.net",
+  USER: "ba3d7a4416513c",
+  PASSWORD: "172d6fc5",
+  DB: "heroku_48fd9b3436559f8"
 };
